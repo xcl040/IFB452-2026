78157e8acb11e5991ac82b59e865f23cb61d2430
xcl040 <xuchen0619@gmail.com>
2026-03-29 16:49:07 +1000

    - modify week 2 code

--- Week 2/Quality_contract.sol
+++ Week 2/Quality_contract.sol
@@ -6,26 +6,29 @@ contract QualityContract{
     uint256 public qualityScore;
 
     modifier onlyStakeholder() {
-        require(msg.sender == singleStakeholder, "Only stakeholder can execute this");
+        require(
+            msg.sender == singleStakeholder, 
+            "Only stakeholder can execute this");
         _;
     
     }
 
     
 
-    event updateQualityScore(uint256 newScore);
 
-    constructor(address initialStakeholder){
-        require(initialStakeholder != address(0), "Invalid intial stakeholder address");
-        singleStakeholder == initialStakeholder;
+    constructor(address initialStakeholder) {
+        require(
+            initialStakeholder != address(0),
+            "Invalid initial stakeholder address"
+        );
+        singleStakeholder = initialStakeholder;
         qualityScore = 0;
-
-
     }
 
-    function updateScore(uint256 newScore) external onlyStakeholder{
+    event updateQualityScore(uint256 newScore);
+
+    function updateScore(uint256 newScore) external onlyStakeholder {
         qualityScore = newScore;
         emit updateQualityScore(newScore);
     }
-
 }
