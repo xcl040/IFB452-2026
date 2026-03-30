51fd259b81a7fd1c2fb5dc20d5abdaae6c1556bd
xcl040 <xuchen0619@gmail.com>
2026-03-30 17:22:41 +1000

    - modified week 4 code

--- Week 3/week03.sol
+++ Week 3/week03.sol
@@ -1,35 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract QualityContract{
-    mapping(address=> bool)public stakeholders;
-    uint256 public qualityScore;
-    uint8 constant stakeholderMAXNumber = 2;
-    uint8 public stakeholderNumber;
+contract QualityContract {
 
-    modifier onlyStakeholder(){
-    require(stakeholders[msg.sender],"Only authorised stakeholder can execute this");
-    _;
-    }
-    modifier exceedTwoStakeholder(uint8 SHnumber){
-        require(SHnumber > stakeholderMAXNumber, "Must not add exceed two stakeholder");
+    mapping(address => bool) public stakeholders;
+    address[] public stakeholderList;
+    uint256 public qualityScore;
+
+    event QualityScoreUpdated(uint256 newScore);
+    event StakeholderAdded(address newStakeholder);
+
+    modifier onlyStakeholder() {
+        require(stakeholders[msg.sender], "Only stakeholder can execute this");
         _;
     }
 
-    constructor(address[] memory initialStakeholders) exceedTwoStakeholder(initialStakeholders.length){
-        for(uint256 i=0; i<initialStakeholders.length;i++){
-            stakeholders[initialStakeholders[i]]=true;
-        }
-        qualityScore=0;
-        stakeholderNumber = initialStakeholders.length;
-    }
-        
+    constructor(address[] memory initialStakeholders) {
+        require(initialStakeholders.length >= 3, "Need at least 3 stakeholders");
 
-        function updateQualityScore(uint256 newScore)external onlyStakeholder{
-            qualityScore=newScore;
+        for (uint i = 0; i < initialStakeholders.length; i++) {
+            address stakeholder = initialStakeholders[i];
+
+            require(stakeholder != address(0), "Invalid address");
+            require(!stakeholders[stakeholder], "Duplicate stakeholder");
+
+            stakeholders[stakeholder] = true;
+            stakeholderList.push(stakeholder);
         }
-        function addStakeholder(address newStakeholder)external onlyStakeholder{
-            stakeholders[newStakeholder]=true;
-        }
+
+        qualityScore = 0;
     }
-    
+
+    function updateScore(uint256 newScore) external onlyStakeholder {
+        qualityScore = newScore;
+        emit QualityScoreUpdated(newScore);
+    }
+
+    function addStakeholder(address newStakeholder) external onlyStakeholder {
+        require(newStakeholder != address(0), "Invalid address");
+        require(!stakeholders[newStakeholder], "Already a stakeholder");
+
+        stakeholders[newStakeholder] = true;
+        stakeholderList.push(newStakeholder);
+
+        emit StakeholderAdded(newStakeholder);
+    }
+
+    function getStakeholderCount() public view returns (uint256) {
+        return stakeholderList.length;
+    }
+
+    function isStakeholder(address user) public view returns (bool) {
+        return stakeholders[user];
+    }
+}
